22acab4d067db56fc433fbc09e2542883ecec09c
yang023 <zjy799281327@qq.com>
2019-02-03 13:12:59 +0800

    分页参数抽象类

---
+++ src/main/java/cn/yang/commons/http/AbstractPageQuery.java
@@ -0,0 +1,30 @@
+package cn.yang.commons.http;
+
+/**
+ * @author yang 2019/2/2
+ */
+public abstract class AbstractPageQuery {
+
+    private Integer page;
+    private Integer size;
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public Integer getIndex() {
+        return (page - 1) * size;
+    }
+}
